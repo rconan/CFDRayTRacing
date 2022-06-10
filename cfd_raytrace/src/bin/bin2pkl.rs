a8c09be9a6e983ac6b06cfd13dcfde7efae2fc0c
Rod Conan <rconan@gmto.org>
2022-06-10 16:14:42 +0000

    BIN2PKL ARGS

--- cfd_raytrace/src/bin/bin2pkl.rs
+++ cfd_raytrace/src/bin/bin2pkl.rs
@@ -1,12 +1,15 @@
-use std::{fs::File, path::Path};
+use std::{env, fs::File, path::Path};
 
 fn main() -> anyhow::Result<()> {
-    let path = Path::new("data/optvol_optvol_3.000000e+02.bin");
-    let data: Vec<f64> = bincode::deserialize_from(File::open(path)?)?;
-    serde_pickle::to_writer(
-        &mut File::create(path.with_extension("pkl"))?,
-        &data,
-        Default::default(),
-    )?;
+    for arg in env::args().skip(1) {
+        let path = Path::new(&arg);
+        println!("{:?}", path);
+        let data: Vec<f64> = bincode::deserialize_from(File::open(path)?)?;
+        serde_pickle::to_writer(
+            &mut File::create(path.with_extension("pkl"))?,
+            &data,
+            Default::default(),
+        )?;
+    }
     Ok(())
 }
